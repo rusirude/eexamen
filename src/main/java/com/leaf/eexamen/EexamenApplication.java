c22c31b4ef62fbcfbeb041eaa2972be995c5b555
rusiru_d <rusiru_d@epiclanka.net>
2021-05-01 11:26:37 +0530

    optimize

--- src/main/java/com/leaf/eexamen/EexamenApplication.java
+++ src/main/java/com/leaf/eexamen/EexamenApplication.java
@@ -2,11 +2,17 @@ package com.leaf.eexamen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EexamenApplication {
+public class EexamenApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EexamenApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(EexamenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EexamenApplication.class, args);
+    }
 }
