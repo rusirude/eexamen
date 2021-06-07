3b1890c58579cb9c29d5795180d064a42b9cd8b8
Rusiru DeSilva <rusiru091@gmail.com>
2021-06-07 12:16:52 +0530

    exam type

--- src/main/resources/static/js/examType.js
+++ src/main/resources/static/js/examType.js
@@ -2,13 +2,13 @@
  * @author: rusiru
  */
 var stepper;
-var trowCount = 0;
-var wrowCount = 0;
+var tRowCount = 0;
+var wRowCount = 0;
 var examTypeTable;
 
 var rowCreator = (type) => {
 
-    let rowCount = type === 't'?++trowCount:++wrowCount;
+    let rowCount = type === 't'?++tRowCount:++wRowCount;
     return `<div class="row body">
 						<div class="form-group col-sm-3">
                             <input type="hidden" name="${type}_ex_tp_id_${rowCount}"/>
@@ -49,9 +49,384 @@ function removeRow(ele,type) {
         .find('button:has(i.fa-minus)').remove();
     $(ele).parent().parent().remove();
 }
+
+/*------------------------------------------- CRUD Functions ------------------*/
+
+var generateFinalObjectForExamType = () => {
+    return {
+        code: $("#code").val() || "",
+        statusCode: $("#status").val() || "",
+        description: $("#description").val() || "",
+        examCategoryCode: $("#examCategory").val() || "",
+        questionCategoryCode: $("#questionCategory").val() || "",
+        tPassMark: $("#t_passmark").val() || 0,
+        wPassMark: $("#w_passmark").val() || 0,
+        tQuestions: generateQuestionModelObjects('t'),
+        wQuestions: generateQuestionModelObjects('w')
+    }
+};
+
+var generateQuestionModelObjects = (type) => {
+    let _r = [];
+    for (let ele of $("#"+type+"Section").find('.row.body')) {
+        let o = {};
+        o.id = $(ele).find('input[type=hidden]').val() || 0;
+        o.typeCode = type === 't'?'THEORIM':'WETGEVING';
+        o.group = $(ele).find(`select[id ^= "${type}_group_"]`).val() || '';
+        o.label = $(ele).find(`select[id ^= "${type}_label_"]`).val() || '';
+        o.noQuestion = $(ele).find('input[type=number]').val() || 0;
+        _r.push(o);
+    }
+    return _r;
+};
+
+var successFunctionForExamType = (data) => {
+    if (data.code === Constant.CODE_SUCCESS) {
+        DialogBox.openMsgBox(data.message, 'success');
+        $("#examTypeFormDiv").hide();
+        $("#examTypeTableDiv").show();
+        examTypeTable.ajax.reload();
+        clearDataForExamType();
+    } else {
+        DialogBox.openMsgBox(data.message, 'error');
+    }
+};
+
+var failedFunctionForExamType = (data) => {
+    DialogBox.openMsgBox("Server Error", 'error');
+};
+
+
+var validatorForExamTypeStepOne = () => {
+    let isValid = true;
+
+    let code = $("#code");
+    let description = $("#description");
+    let status = $("#status");
+    let questionCategory = $("#questionCategory");
+    let examCategory = $("#examCategory");
+
+    if (!code.val()) {
+        InputsValidator.inlineEmptyValidation(code);
+        isValid = false;
+    }
+    if (!description.val()) {
+        InputsValidator.inlineEmptyValidation(description);
+        isValid = false;
+    }
+    if (!status.val()) {
+        InputsValidator.inlineEmptyValidationSelect(status);
+        isValid = false;
+    }
+    if (!status.val()) {
+        InputsValidator.inlineEmptyValidationSelect(status);
+        isValid = false;
+    }
+    if (!questionCategory.val()) {
+        InputsValidator.inlineEmptyValidationSelect(questionCategory);
+        isValid = false;
+    }
+    if (!examCategory.val()) {
+        InputsValidator.inlineEmptyValidationSelect(examCategory);
+        isValid = false;
+    }
+    return isValid;
+};
+
+var validatorForExamTypeStepOne = () => {
+    let isValid = true;
+
+    let code = $("#code");
+    let description = $("#description");
+    let status = $("#status");
+    let questionCategory = $("#questionCategory");
+    let examCategory = $("#examCategory");
+
+    if (!code.val()) {
+        InputsValidator.inlineEmptyValidation(code);
+        isValid = false;
+    }
+    if (!description.val()) {
+        InputsValidator.inlineEmptyValidation(description);
+        isValid = false;
+    }
+    if (!status.val()) {
+        InputsValidator.inlineEmptyValidationSelect(status);
+        isValid = false;
+    }
+    if (!status.val()) {
+        InputsValidator.inlineEmptyValidationSelect(status);
+        isValid = false;
+    }
+    if (!questionCategory.val()) {
+        InputsValidator.inlineEmptyValidationSelect(questionCategory);
+        isValid = false;
+    }
+    if (!examCategory.val()) {
+        InputsValidator.inlineEmptyValidationSelect(examCategory);
+        isValid = false;
+    }
+    return isValid;
+};
+
+
+var validatorForExamQuestionModel = (type) => {
+    let isValid = true;
+
+
+    for (let ele of $("#"+type+"Section").find('.row.body')) {
+
+        if (!$(ele).find(`select[id ^= "${type}_group_"]`).val()) {
+            InputsValidator.inlineEmptyValidationSelect($(ele).find(`select[id ^= "${type}_group_"]`));
+            isValid = false;
+        }
+        if (!$(ele).find(`select[id ^= "${type}_label_"]`).val()) {
+            InputsValidator.inlineEmptyValidationSelect($(ele).find(`select[id ^= "${type}_label_"]`));
+            isValid = false;
+        }
+
+        if(! parseInt((ele).find('input[type=number]').val()||0)){
+            InputsValidator.inlineEmptyValidationNumber((ele).find('input[type=number]'));
+            isValid = false;
+        }
+    }
+
+    return isValid;
+};
+
+
+var saveForExamType = () => {
+    if (validatorForExamQuestionModel('w')) {
+        let url = "/examType/save";
+        let method = "POST";
+
+        callToserver(url, method, generateFinalObjectForExamType(), successFunctionForExamType, failedFunctionForExamType);
+    }
+
+};
+
+var updateForExamType = () => {
+    if (validatorForExamQuestionModel('w')) {
+        let url = "/examType/update";
+        let method = "POST";
+
+        callToserver(url, method, generateFinalObjectForExamType(), successFunctionForExamType, failedFunctionForExamType);
+    }
+};
+
+var deleteForExamType = () => {
+    let url = "/examType/delete";
+    let method = "POST";
+
+    callToserver(url, method, generateFinalObjectForExamType(), successFunctionForExamType, failedFunctionForExamType);
+};
+
+var findDetailByCodeForExamType = (code, callback) => {
+    let successFunction = (data) => {
+        if (data.code === Constant.CODE_SUCCESS) {
+            if (callback) {
+                callback(data.data);
+            }
+        } else {
+            DialogBox.openMsgBox(data.message, 'error');
+        }
+    };
+    let failedFunction = (data) => {
+        DialogBox.openMsgBox("Server Error", 'error');
+    };
+    let url = "/exmType/loadExamTypeByCode";
+    let method = "POST";
+    callToserver(url, method, {code: code}, successFunction, failedFunction);
+};
+
+/*-------------------------------- Reference Data , Data Table and Common --------------------*/
+var populateFormForExamType = (data) => {
+    if (data) {
+        $("#code").val(data.code || "");
+        $("#description").val(data.description || "");
+        $("#status").val(data.statusCode || "");
+        $("#examCategory").val(data.examCategoryCode || "");
+        $("#questionCategory").val(data.questionCategory || "");
+        $("#t_passmark").val(data.tPassMark || 0);
+        $("#w_passmark").val(data.wPassMark || 0);
+        $("#type").val(data.type || "");
+        $("#group").val(data.group || "");
+        $("#label").val(data.label || "");
+        if ((data.tQuestions || []).length) {
+            tRowCount = 0;
+            $("#tSection")
+                .find('.row.body').remove();
+
+            for (let e of data.tQuestions) {
+                let row = $(rowCreator());
+                row.find("input[type=hidden]").val(e.id);
+                row.find(`select[id ^= "${type}_group_"]`).val(e.group);
+                row.find(`select[id ^= "${type}_group_"]`).val(e.label);
+                row.find('input[type=number]').val(e.noQuestion);
+                $("#tSection").append(row);
+            }
+        }
+        if ((data.wQuestions || []).length) {
+            tRowCount = 0;
+            $("#wSection")
+                .find('.row.body').remove();
+
+            for (let e of data.wQuestions) {
+                let row = $(rowCreator());
+                row.find("input[type=hidden]").val(e.id);
+                row.find(`select[id ^= "${type}_group_"]`).val(e.group);
+                row.find(`select[id ^= "${type}_group_"]`).val(e.label);
+                row.find('input[type=number]').val(e.noQuestion);
+                $("#wSection").append(row);
+            }
+        }
+
+
+    }
+};
+
+var loadReferenceDataForExamType = (callback) => {
+    $.ajax({
+        type: "POST",
+        url: "/examType/loadRefDataForExamType",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+
+            if (data.code === Constant.CODE_SUCCESS) {
+                for (let s of data.data.status || []) {
+                    $("#status").append(`<option value="${s.code}">${s.description}</option>`);
+                }
+                for (let sc of data.data.questionCategory || []) {
+                    $("#questionCategory").append(`<option value="${sc.code}">${sc.description}</option>`);
+                }
+                for (let sc of data.data.examCategory || []) {
+                    $("#examCategory").append(`<option value="${sc.code}">${sc.description}</option>`);
+                }
+
+                if (callback) {
+                    callback();
+                }
+            } else {
+                DialogBox.openMsgBox("System Failer Occur....! :-(", 'error');
+            }
+
+
+        },
+        failure: function (errMsg) {
+            DialogBox.openMsgBox(errMsg, 'error');
+        }
+    });
+};
+
+var loadExamTypeTable = () => {
+    questionTable = $('#examTypeTable').DataTable({
+        ajax: {
+            url: "/examType/loadExamTypes",
+            contentType: "application/json",
+            type: "POST",
+            data: function (d) {
+                return JSON.stringify(createCommonDataTableRequset(d));
+            }
+        },
+        paging: true,
+        lengthChange: false,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: false,
+        processing: true,
+        serverSide: true,
+        scrollX: true,
+        columns: [
+            {data: "code", name: "code"},
+            {data: "description", name: "description"},
+            {data: "statusDescription", name: "status"},
+            {data: "createdBy", name: "createdBy"},
+            {data: "createdOn", name: "createdOn"},
+            {
+                data: "code",
+                render: function (data, type, full) {
+                    return `<button onClick="updateIconClickForExamType('${data}')" type="button" class="btn btn-outline-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="Update">
+														<i class="fa fa-pencil-alt"></i>
+													</button>
+													<button onClick="deleteIconClickForExamType('${data}')" type="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="bottom" title="Delete">
+														<i class="fa fa-trash-alt"></i>
+													</button>`;
+                }
+            }
+        ]
+    });
+};
+
+
+var clearDataForExamType = () => {
+    let code = $("#code");
+    let description = $("#description");
+    let status = $("#status");
+    let examCategory = $("#examCategory");
+    let questionCategory = $("#questionCategory");
+    let tPassmark = $("#t_passmark");
+    let wPassmark = $("#w_passmark");
+
+    $("#btnSave").show();
+    $("#btnUpdate").hide();
+    $("#btnDelete").hide();
+
+    code.prop("disabled", false);
+    description.prop("disabled", false);
+    status.prop("disabled", false);
+    examCategory.prop("disabled", false);
+    questionCategory.prop("disabled", false);
+    tPassmark.prop("disabled", false);
+    wPassmark.prop("disabled", false);
+
+    tRowCount = 0;
+    $("#tSection")
+        .find('.row.body').remove();
+
+
+    $("#tSection").append(rowCreator('t'));
+
+    $("#tSection")
+        .find('.row.body:first')
+        .find('button:has(i.fa-minus)').remove();
+
+    wRowCount = 0;
+    $("#wSection")
+        .find('.row.body').remove();
+
+
+    $("#wSection").append(rowCreator('t'));
+
+    $("#wSection")
+        .find('.row.body:first')
+        .find('button:has(i.fa-minus)').remove();
+
+    InputsValidator.removeInlineValidation(code);
+    InputsValidator.removeInlineValidation(description);
+    InputsValidator.removeInlineValidation(status);
+    InputsValidator.removeInlineValidation(examCategory);
+    InputsValidator.removeInlineValidation(questionCategory);
+    InputsValidator.removeInlineValidation(tPassmark);
+    InputsValidator.removeInlineValidation(wPassmark);
+
+    code.val("");
+    description.val("");
+    status.val("");
+    examCategory.val("");
+    questionCategory.val("");
+    tPassmark.val(0);
+    wPassmark.val(0);
+    stepper.previous();
+    stepper.previous();
+
+
+};
+
 /*-------------------------------- Inline Event  ----------------------*/
 var clickAddForExamType = () => {
-    //clearDataForExamType();
+    clearDataForExamType();
     $("#formHeading").html("Add ExamType");
     $("#examTypeTableDiv").hide();
     $("#examTypeFormDiv").show();
@@ -81,12 +456,15 @@ var deleteIconClickForExamType = (code) => {
         $("#code").prop("disabled", true);
         $("#description").prop("disabled", true);
         $("#status").prop("disabled", true);
-        $("#type").prop("disabled", true);
-        $("#group").prop("disabled", true);
-        $("#label").prop("disabled", true);
-        $('#examTypeCategory').select2("enable",true);
+        $("#examCategory").prop("disabled", true);
+        $("#questionCategory").prop("disabled", true);
+        $("#t_passmark").prop("disabled", true);
+        $("#w_passmark").prop("disabled", true);
         $("#formHeading").html("Delete ExamType");
-        $("#answerSection")
+        $("#tSection")
+            .find('.row.body')
+            .find("input").prop("disabled", true);
+        $("#wSection")
             .find('.row.body')
             .find("input").prop("disabled", true);
         $("#examTypeTableDiv").hide();
@@ -97,25 +475,20 @@ var deleteIconClickForExamType = (code) => {
 };
 
 var cancelForm = () => {
-    //clearDataForExamType();
+    clearDataForExamType();
     $("#examTypeFormDiv").hide();
     $("#examTypeTableDiv").show();
 };
 var clickNextOne = () => {
-    if (true) {
+
+    if (validatorForExamTypeStepOne()) {
         stepper.next();
     }
-    //if (validatorForExamTypeStepOne()) {
-    //    stepper.next();
-    //}
 };
 var clickNextTwo = () => {
-    if (true) {
+    if (validatorForExamQuestionModel('t')) {
         stepper.next();
     }
-    //if (validatorForExamTypeStepTwo()) {
-    //    stepper.next();
-    //}
 };
 
 /*-------------------------------- Dynamic Event  ----------------------*/
@@ -145,15 +518,8 @@ var evenBinderForExamType = () => {
 $(document).ready(() => {
     let _callback_1 = () => {
         stepper = new Stepper($('.bs-stepper')[0]);
-        //$('#examTypeCategory').select2();
-        //$('#examTypeCategory').on('select2:select', function (e) {
-        //    let _element = $(this);
-        //    _element.removeClass('is-invalid');
-        //    _element.parent().find("span.error").html("");
-        //});
     };
-    _callback_1();
-    //loadReferenceDataForExamType(_callback_1);
-    //loadExamTypeTable();
+    loadReferenceDataForExamType(_callback_1);
+    loadExamTypeTable();
     evenBinderForExamType();
 });
